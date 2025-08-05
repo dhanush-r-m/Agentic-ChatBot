418b4ebf8ae18ca9584fc51263453861d563f8c8
dhanush-r-m <Dhanushmoolemane@gmail.com>
2025-08-05 12:34:01 +0530

    Node Analyzer

--- src/Langraph/nodes/basic_chatbot_node.py
+++ src/Langraph/nodes/basic_chatbot_node.py
@@ -6,8 +6,8 @@ class BasicChatbotNode:
     A basic chatbot node that can be used in a Langraph workflow.
     This node is designed to handle simple chat interactions.
     """
-    def __init__(self):
-        self.llm = model 
+    def __init__(self, model):
+        self.llm = model
 
     def process(self, state : State) -> dict:
         """
