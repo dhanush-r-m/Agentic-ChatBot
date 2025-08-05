84d9d409d44adaec3839a000e22482f4f561ee79
dhanush-r-m <Dhanushmoolemane@gmail.com>
2025-08-05 22:02:47 +0530

    Updated With Web Functionality

--- src/langgraphagenticai/tools/search_tool.py
+++ src/langgraphagenticai/tools/search_tool.py
@@ -1,19 +1,16 @@
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import ToolNode
 
-
 def get_tools():
     """
-    Returns a list of tools for the chatbot.
-    Currently, it includes a search tool using Tavily.
+    Return the list of tools to be used in the chatbot
     """
-    tools = [TavilySearchResults(max_results=2)]
+    tools=[TavilySearchResults(max_results=2)]
     return tools
 
 def create_tool_node(tools):
     """
-    Creates a ToolNode for the chatbot.
-    This node integrates the search tool into the chatbot's workflow.
+    creates and returns a tool node for the graph
     """
-
     return ToolNode(tools=tools)
+
