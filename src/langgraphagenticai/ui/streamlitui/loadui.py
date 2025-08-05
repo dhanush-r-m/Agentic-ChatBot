facf34e7c4d86c59cc8b1486163f993bf0f0e3cc
dhanush-r-m <Dhanushmoolemane@gmail.com>
2025-08-05 19:25:09 +0530

    ChatBot-Web

--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -33,4 +33,7 @@ class LoadStreamlitUI:
             ## USecase selection
             self.user_controls["selected_usecase"]=st.selectbox("Select Usecases",usecase_options)
 
+            if self.user_controls["selected_usecase"] == 'ChatBot with Web':
+                self.user_controls["TAVILY_API_KEY"] = st.session_state["TAVILY_API_KEY"]=st.text_input("Tavily API Key",type="password")
+
         return self.user_controls
